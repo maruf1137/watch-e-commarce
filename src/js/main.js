cc98b451a093cdd58ddd08d67fb1949a4d197210
Maruf Mia <mmaruf1137@gmail.com>
2022-09-07 23:33:11 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-
 // sidebar
 
 const openSidebarBtn = document.querySelector('.btn-bars');
@@ -13,4 +12,20 @@ closeSidebarBtn.addEventListener('click', () => {
 	main.classList.remove('show-sidebar');
 });
 
-// 
+// auth popup
+const auth = document.querySelector('.auth');
+const openAuthPopup = document.querySelectorAll('.btn-auth');
+const closeAuthPopup = document.querySelector('.close-popup');
+const AuthOverlay = document.querySelector('.auth .overlay');
+
+openAuthPopup.forEach((e) => {
+	e.addEventListener('click', () => {
+		auth.classList.toggle('show-popup');
+	});
+});
+
+function closeAuthPopupFunc() {
+	auth.classList.remove('show-popup');
+}
+closeAuthPopup.addEventListener('click', closeAuthPopupFunc);
+AuthOverlay.addEventListener('click', closeAuthPopupFunc);
